f2e8750521049d667dd3a6fef919d81c4e0eba3c
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-05-24 14:54:39 +0800

    init and disconnect prisma

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,44 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/lib/prisma";
-import { compare } from "bcrypt";
+// import NextAuth, { type NextAuthOptions } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import prisma from "@/lib/prisma";
+// import { User as UserModel } from "@prisma/client";
+// import { compare } from "bcrypt";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        const { email, password } = credentials ?? {}
-        if (!email || !password) {
-          throw new Error("Missing username or password");
-        }
-        const user = await prisma.user.findUnique({
-          where: {
-            email,
-          },
-        });
-        // if user doesn't exist or password doesn't match
-        if (!user || !(await compare(password, user.password))) {
-          throw new Error("Invalid username or password");
-        }
-        return user;
-      },
-    }),
-  ],
-};
+// // removed on mongoose integration
+// declare module "next-auth" {
+//   interface User extends UserModel {
+//     id: number;
+//   }
+// }
 
-const handler = NextAuth(authOptions);
+// export const authOptions: NextAuthOptions = {
+//   providers: [
+//     CredentialsProvider({
+//       credentials: {
+//         email: { label: "Email", type: "email" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         const { email, password } = credentials ?? {};
+//         if (!email || !password) {
+//           throw new Error("Missing username or password");
+//         }
+//         // replace this with mongoose
+//         const user = await prisma.user.findUnique({
+//           where: {
+//             email,
+//           },
+//         });
+//         // if user doesn't exist or password doesn't match
+//         if (!user || !(await compare(password, user.password))) {
+//           throw new Error("Invalid username or password");
+//         }
+//         return user;
+//       },
+//     }),
+//   ],
+// };
 
-export { handler as GET, handler as POST };
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
--- app/protected-prisma/page.tsx
+++ app/protected-prisma/page.tsx
@@ -1,4 +1,4 @@
-import SignOut from "@/components/sign-out";
+import SignOut from "@/components/sign-out-prisma";
 
 export default function Home() {
   return (
