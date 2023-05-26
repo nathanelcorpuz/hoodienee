51b50895fcf7c5024d39ba90d0e5d71b0f783545
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-05-26 09:33:13 +0800

    reusable input with icon

--- app/search/_components/Search.tsx
+++ app/search/_components/Search.tsx
@@ -1,29 +1,17 @@
-// WIP
-
-import Input from "@/components/Input";
+import InputWithIcon from "@/components/inputs/InputWithIcon";
 import icon from "@/public/icons/search.png";
-import Image from "next/image";
 
 export default function Search() {
   return (
     <div>
-      <div className="flex p-2">
-        <Input
-          props={{
-            type: "text",
-            id: "search",
-            placeholder: "Search for anything",
-          }}
-        />
-        <div
-          className="
-        flex h-[40px] w-[40px] 
-        items-center justify-center
-        border border-s-0 border-gray-600"
-        >
-          <Image src={icon} alt="search icon" width={15} height={15} />
-        </div>
-      </div>
+      <InputWithIcon
+        props={{
+          type: "text",
+          id: "search",
+          placeholder: "Search for anything",
+        }}
+        icon={icon}
+      />
     </div>
   );
 }
