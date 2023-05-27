4a6f67fa86325f594d5a6dfb82dd12a4ce4acde9
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-05-27 14:12:10 +0800

    collection ui

--- app/collections/[slug]/page.tsx
+++ app/collections/[slug]/page.tsx
@@ -1,10 +1,23 @@
+"use client";
+
+import Button from "@/components/Button";
+import ProductCard from "@/components/ProductCard";
+import HeadingText from "@/components/texts/HeadingText";
 import PageWrapper from "@/components/wrappers/PageWrapper";
-import capitalize from "utils/capitalize";
+import { sampleFetchedProducts } from "@/lib/constants";
 
 export default function Collection({ params }: { params: { slug: string } }) {
   return (
-    <PageWrapper>
-      <div>{capitalize(params.slug)}</div>
+    <PageWrapper className="gap-[100px]">
+      <div className="sticky top-[40.5px] z-[100] border-b border-gray-700 py-2 text-center dark:bg-black">
+        <HeadingText>{params.slug}</HeadingText>
+      </div>
+      <div className="flex flex-col items-center gap-[50px]">
+        {sampleFetchedProducts.map((product) => (
+          <ProductCard />
+        ))}
+      </div>
+      <Button className="w-[fit-content]">Load more</Button>
     </PageWrapper>
   );
 }
