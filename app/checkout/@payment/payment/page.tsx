7bdcdbf13aef65cbde989f7f575d35ea68852989
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-05-30 11:19:27 +0800

    ecommerce

--- app/checkout/@payment/payment/page.tsx
+++ app/checkout/@payment/payment/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+
+import Button from "@/components/Button";
+import Input from "@/components/inputs/Input";
+import FormWrapper from "@/components/wrappers/FormWrapper";
 import PageWrapper from "@/components/wrappers/PageWrapper";
 
 export default function PaymentPage() {
-  return <PageWrapper>Checkout payment page</PageWrapper>;
+  return (
+    <PageWrapper>
+      <FormWrapper>
+        <Input label="Card Number" />
+        <Input label="CVC" />
+        <Input label="Expiration Date" />
+        <Button props={{ onClick: (e) => e.preventDefault() }}>
+          Submit Order
+        </Button>
+      </FormWrapper>
+    </PageWrapper>
+  );
 }
